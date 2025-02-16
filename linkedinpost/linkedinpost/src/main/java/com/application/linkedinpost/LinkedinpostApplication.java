7f5ca40cd2279c20f120d26fe288deb7a3334f2d
Yahoshuva <pasupuletiyahoshuva1999@gmail.com>
2025-02-16 17:27:51 +0530

    chore:set up backend & frontend using spring Initializr & vite

---
+++ linkedinpost/linkedinpost/src/main/java/com/application/linkedinpost/LinkedinpostApplication.java
@@ -0,0 +1,13 @@
+package com.application.linkedinpost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedinpostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkedinpostApplication.class, args);
+	}
+
+}
